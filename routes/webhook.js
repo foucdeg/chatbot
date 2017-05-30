0bc963af89a2f05fbba6761d97cbdcb07bc2f943
Lucas Fauchille <fauchi_l@epita.fr>
2017-05-30 20:41:56 +0200

    Echo du message

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,4 +1,7 @@
-var authenticate = require('../server/chatService').authenticate;
+var chatService = require('../server/chatService');
+var authenticate = chatService.authenticate;
+var sendTextMessage = chatService.sendTextMessage;
+
 var express = require('express');
 var router = express.Router();
 
@@ -8,7 +11,27 @@ router.get('/', function(req, res, next) {
     res.status(200).send(req.query['hub.challenge']);
   }
   else {
-    res.sendStatus(403); 
+    res.sendStatus(403);
+  }
+});
+
+router.post('/', function (req, res) {
+  var data = req.body;
+
+  if (data.object === 'page') {
+
+    data.entry.forEach(function(entry) {
+
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          sendTextMessage(event.sender.id, event.message.text);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    res.sendStatus(200);
   }
 });
 
